cc0c36c37ace986412f14e3b6b124b9698c978ec
nosoosso <nosoosso.null@gmail.com>
2016-07-27 15:40:41 +0900

    ブラウザからログインできるようにした

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // define the home page route
 router.get('/', function(req, res) {
-  res.send('index page');
+  res.render('index');
 });
 
 router.get('/auth', function(req, res) {
