cd3c1c763f60589d71e468f40856a5f306fcff40
nosoosso <nosoosso.null@gmail.com>
2016-08-02 19:02:40 +0900

    ログアウトできるようにした

--- front_end/src/js/components/index.jsx
+++ front_end/src/js/components/index.jsx
@@ -11,7 +11,7 @@ export class Index extends React.Component {
     };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     const firebaseConf = config.firebase.config;
     firebase.initializeApp(firebaseConf);
     firebase.auth().onAuthStateChanged(user => {
@@ -27,12 +27,34 @@ export class Index extends React.Component {
     });
   }
 
+  handleLogout(e) {
+    e.preventDefault();
+
+    firebase.auth().signOut()
+      .then(() => {
+        location.href = config.url;
+      })
+      .catch(() => {
+        location.href = config.url;
+      });
+  }
+
   render() {
-    if (this.state.user !== null) {
+    if (this.state.user !== null && !this.state.user.isAnonymous) {
       return (
         <div>
           <div>ログイン中： {this.state.user.email}</div>
           <div><a href="screen.html">スクリーン</a></div>
+          <div><a href="" onClick={this.handleLogout}>ログアウト</a></div>
+        </div>
+      );
+    } else if (this.state.user !== null && this.state.user.isAnonymous) {
+      // 匿名でログイン中
+      return (
+        <div>
+          <div>アノニマスログイン中</div>
+          <div>スクリーン表示不可</div>
+          <div><a href="" onClick={this.handleLogout}>ログアウト</a></div>
         </div>
       );
     } else {
