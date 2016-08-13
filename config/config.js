8ffe61b4555c2740dc4ffd9eddf8ccdc9df729df
mpppk <niboshiporipori@gmail.com>
2016-08-13 17:15:21 +0900

    template literalを削除
    babelがtemplate literalをそのまま吐いてしまい、webpack --releaseするとUglifyJSがこける?
    とりあえずtemplate literalを使わないようにした

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
-firebaseProjectId = 'sample-5f412'
+const firebaseProjectId = 'sample-5f412';
 module.exports = {
   url: '',
   firebase: {
     config: {
       apiKey: 'AIzaSyDNBvfpc07ND30lVfjcPYE6VddcVd237mQ',
-      authDomain: `${firebaseProjectId}.firebaseapp.com`,
-      databaseURL: `https://${firebaseProjectId}.firebaseio.com`,
+      authDomain: firebaseProjectId + '.firebaseapp.com',
+      databaseURL: 'https://' + firebaseProjectId + '.firebaseio.com',
+      // authDomain: `${firebaseProjectId}.firebaseapp.com`,
+      // databaseURL: `https://${firebaseProjectId}.firebaseio.com`,
     },
     serviceAccount: {
       projectId: firebaseProjectId || process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
