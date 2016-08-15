75b4a618da1a7fa6ab292f6b89ccaeddaa3697ed
mpppk <niboshiporipori@gmail.com>
2016-08-15 13:45:47 +0900

    Fix #6

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(bodyParser.json());
 
 app.use('/', index);
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!');
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
