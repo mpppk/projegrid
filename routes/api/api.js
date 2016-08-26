110e650aeafaddd9405b0a199b3c177800131264
nosoosso <nosoosso.null@gmail.com>
2016-08-27 00:00:13 +0900

    スクリーン表示時にスクリーントークンを更新するようにした
    スクリーントークンが設定されていないときにスクリーンを表示しようとすると表示に失敗してしまうことになる。
    これを避けるため、スクリーンユーザーを登録した後はどこかのタイミングでスクリーントークを設定する必要がある。
    そこでスクリーン表示時には毎回トークンを設定することにした。

--- routes/api/api.js
+++ routes/api/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const screen = require('./screen/api.js');
+const token = require('./token/api.js');
 
 router.use('/screen', screen);
+router.use('/token', token);
 
 module.exports = router;
