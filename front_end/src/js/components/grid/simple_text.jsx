6c1e39e53a8be6e817050fafa7b34bea9f0769fb
nosoosso <nosoosso.null@gmail.com>
2016-09-04 21:41:22 +0900

    SimpleTextグリッドのデータ構造を変更
    今まで普通にgrid1: "hoge" みたいに保存していたのを grid1:{type:"SIMPLE_TEXT, text: "hoge"} とするようにした

--- front_end/src/js/components/grid/simple_text.jsx
+++ front_end/src/js/components/grid/simple_text.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 export default class SimpleText extends React.Component {
   render() {
     return (
-      <div>{this.props.data}</div>
+      <div>{this.props.data.text}</div>
     );
   }
 }
 
 SimpleText.propTypes = {
-  data: React.PropTypes.string.isRequired,
+  data: React.PropTypes.shape({
+    text: React.PropTypes.string.isRequired,
+  }),
 };
